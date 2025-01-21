a38d5ecafaad699ce1b09fb89a83897b3c706504
褚学琛 <eemmmml@chuxuechendeMacBook-Pro.local>
2025-01-22 01:01:13 +0800

    chapter8 test1

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+// 给定一组整数，使用动态数组来计算该组整数中的平均数、中
+// 位数（对数组进行排序后位于中间的值）及众数（出现次数最多的值；哈希映射可以在这里帮上忙）
 use std::collections::HashMap;
 
 fn main() {
-    let nums = vec![1, 2, 2, 2, 3, 4, 5, 6, 6, 6];
+    let mut nums = vec![1, 2, 2, 2, 3, 4, 5, 6, 6, 6, 2, 4, 6, 6];
+    nums.sort();
     let mut sum = 0;
     let mut statistic = HashMap::new();
 
@@ -25,7 +28,15 @@ fn main() {
         }
     }
 
+    let mut mid: f64 = 0.0;
+    if len % 2 == 0 {
+        mid = ((nums[len / 2] as f64) + (nums[len / 2 - 1] as f64)) / (2 as f64);
+    } else {
+        mid = nums[len / 2] as f64;
+    }
+
     println!("Average is {}", average);
+    println!("Mid is {}", mid);
     print!("Max Value: ");
     for i in result {
         print!("{} ", i);
